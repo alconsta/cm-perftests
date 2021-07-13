f91771d5196422f636bb384811e327d7aba18db6
alconsta <48789259+alconsta@users.noreply.github.com>
2021-07-13 23:32:36 +0300

    user-workflow

--- test-cm2.py
+++ test-cm2.py
@@ -1,18 +1,21 @@
     from locust import HttpLocust, TaskSet, task
     from random import randint, randrange
-
+    import time
     class UserTasks(TaskSet):
         @task
         def user_workflow(self):
             heartbeats = randint(5, 100)        
             self.client.get("/v2/metadata", auth=("ccc",""))
-            response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
-            location = response.headers['Location']
-            new_uri = "/v2/sessions/test_mvpd/user_test" + '/' + location
-            print('location is ', location)
-            for i in range(heartbeats):
-                hb_response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
+            init_session_response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
+            session_id = init_session_response.headers['Location']
+            new_uri = "/v2/sessions/test_mvpd/user_test" + '/' + session_id
+            print('location is ', session_id)
+            for i in range(1, heartbeats):
+                time.sleep(60)
+                hb_response = self.client.post(new_uri, auth=("ccc",""))
                 print(hb_response.content)
+            del_session_response = self.client.post(new_uri, auth=("ccc",""))
+            print(del_session_response.content)
         @task
         def stats(self):
             self.client.get("/")
