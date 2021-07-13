a1e61042402ed0ad42c77d810e3902ae9083fb3f
alconsta <48789259+alconsta@users.noreply.github.com>
2021-07-13 22:56:03 +0300

    test with heartbeat

--- test-cm2.py
+++ test-cm2.py
@@ -1,13 +1,20 @@
-from locust import HttpLocust, TaskSet, task
-class UserTasks(TaskSet):
-    @task
-    def index(self):
-        self.client.get("/v2/metadata", auth=("ccc",""))
-        response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
-        location = response.headers['Location']
-        print('location is ', location )
-    @task
-    def stats(self):
-        self.client.get("/")
-class WebsiteUser(HttpLocust):
-    task_set = UserTasks
+    from locust import HttpLocust, TaskSet, task
+    from random import randint, randrange
+
+    class UserTasks(TaskSet):
+        @task
+        def user_workflow(self):
+            heartbeats = randint(5, 100)        
+            self.client.get("/v2/metadata", auth=("ccc",""))
+            response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
+            location = response.headers['Location']
+            new_uri = "/v2/sessions/test_mvpd/user_test" + '/' + location
+            print('location is ', location)
+            for i in range(heartbeats):
+                hb_response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
+                print(hb_response.body.to_string())
+        @task
+        def stats(self):
+            self.client.get("/")
+    class WebsiteUser(HttpLocust):
+        task_set = UserTasks
