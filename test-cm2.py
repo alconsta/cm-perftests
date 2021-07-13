44b02d9901c17817e186c59cc14d95c8b1d0d0ad
alconsta <48789259+alconsta@users.noreply.github.com>
2021-07-13 20:02:53 +0300

    Update test-cm2.py

--- test-cm2.py
+++ test-cm2.py
@@ -3,7 +3,7 @@
         @task
         def index(self):
             self.client.get("/v2/metadata", auth=("ccc",""))
-            response = self.client.post("/v2/sessions/test_mvpd/user_test")
+            response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
             print("Response body is: ", response)
             print(response.content.decode().find('Location'))
         @task
