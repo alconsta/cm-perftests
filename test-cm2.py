10ecc33eb1f3a6ffe54c82c164858b7274e63a47
alconsta <48789259+alconsta@users.noreply.github.com>
2021-07-13 22:54:28 +0300

    test without indent

--- test-cm2.py
+++ test-cm2.py
@@ -1,13 +1,13 @@
-    from locust import HttpLocust, TaskSet, task
-    class UserTasks(TaskSet):
-        @task
-        def index(self):
-            self.client.get("/v2/metadata", auth=("ccc",""))
-            response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
-            location = response.headers['Location']
-            print('location is ', location )
-        @task
-        def stats(self):
-            self.client.get("/")
-    class WebsiteUser(HttpLocust):
-        task_set = UserTasks
+from locust import HttpLocust, TaskSet, task
+class UserTasks(TaskSet):
+    @task
+    def index(self):
+        self.client.get("/v2/metadata", auth=("ccc",""))
+        response = self.client.post("/v2/sessions/test_mvpd/user_test", auth=("ccc",""))
+        location = response.headers['Location']
+        print('location is ', location )
+    @task
+    def stats(self):
+        self.client.get("/")
+class WebsiteUser(HttpLocust):
+    task_set = UserTasks
