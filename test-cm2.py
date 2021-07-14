d7d383297e8c517049f9f5d1bc90cef7043217ab
alconsta <48789259+alconsta@users.noreply.github.com>
2021-07-14 14:40:02 +0300

    without indent

--- test-cm2.py
+++ test-cm2.py
@@ -1,33 +1,33 @@
-    from locust import HttpLocust, TaskSet, task
-    from random import randrange, choices
-    import time, string
-    class UserTasks(TaskSet):
-        def generate_subject(self):
-            length = randrange(14, 38)
-            subejctid = ''.join(choices(string.ascii_letters+string.digits+'-',k=length))
-            return subejctid
-        @task
-        def user_workflow(self):
-            heartbeats = randrange(2, 5)
-            heartbeat_time = 10
-            mvpd = "test_mvpd"
-            userid = self.generate_subject()
-            initial_uri = "/v2/sessions/" + mvpd + "/" + userid
-            print('number of heartbeats', heartbeats)
-            self.client.get("/v2/metadata", auth=("ccc",""))
-            init_session_response = self.client.post(initial_uri, auth=("ccc",""))
-            session_id = init_session_response.headers['Location']
-            new_uri = initial_uri + '/' + session_id
-            print('location is ', session_id)
-            for i in range(1, heartbeats):
-                time.sleep(heartbeat_time)
-                hb_response = self.client.post(new_uri, auth=("ccc",""))
-                print('hb response is ', hb_response)
-            time.sleep(heartbeat_time)    
-            del_session_response = self.client.delete(new_uri, auth=("ccc",""))
-            print('del respone is ', del_session_response)
-        @task
-        def stats(self):
-            self.client.get("/")
-    class WebsiteUser(HttpLocust):
-        task_set = UserTasks
+from locust import HttpLocust, TaskSet, task
+from random import randrange, choices
+import time, string
+class UserTasks(TaskSet):
+    def generate_subject(self):
+        length = randrange(14, 38)
+        subejctid = ''.join(choices(string.ascii_letters+string.digits+'-',k=length))
+        return subejctid
+    @task
+    def user_workflow(self):
+        heartbeats = randrange(2, 5)
+        heartbeat_time = 10
+        mvpd = "test_mvpd"
+        userid = self.generate_subject()
+        initial_uri = "/v2/sessions/" + mvpd + "/" + userid
+        print('number of heartbeats', heartbeats)
+        self.client.get("/v2/metadata", auth=("ccc",""))
+        init_session_response = self.client.post(initial_uri, auth=("ccc",""))
+        session_id = init_session_response.headers['Location']
+        new_uri = initial_uri + '/' + session_id
+        print('location is ', session_id)
+        for i in range(1, heartbeats):
+            time.sleep(heartbeat_time)
+            hb_response = self.client.post(new_uri, auth=("ccc",""))
+            print('hb response is ', hb_response)
+        time.sleep(heartbeat_time)    
+        del_session_response = self.client.delete(new_uri, auth=("ccc",""))
+        print('del respone is ', del_session_response)
+    @task
+    def stats(self):
+        self.client.get("/")
+class WebsiteUser(HttpLocust):
+    task_set = UserTasks
