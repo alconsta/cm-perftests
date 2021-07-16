b89ec57a597651b42b5dd79773ffc89a504925cd
alconsta <48789259+alconsta@users.noreply.github.com>
2021-07-16 09:39:55 +0300

    remove print

--- test-cm2.py
+++ test-cm2.py
@@ -13,18 +13,14 @@ class UserTasks(TaskSet):
         mvpd = "test_mvpd"
         userid = self.generate_subject()
         initial_uri = "/v2/sessions/" + mvpd + "/" + userid
-        print('number of heartbeats', heartbeats)
         self.client.get("/v2/metadata", auth=("ccc",""))
         init_session_response = self.client.post(initial_uri, auth=("ccc",""))
         session_id = init_session_response.headers['Location']
         new_uri = initial_uri + '/' + session_id
-        print('location is ', session_id)
         for i in range(1, heartbeats):
             time.sleep(heartbeat_time)
             hb_response = self.client.post(new_uri, auth=("ccc",""))
-            print('hb response is ', hb_response)
         time.sleep(heartbeat_time)    
         del_session_response = self.client.delete(new_uri, auth=("ccc",""))
-        print('del respone is ', del_session_response)
 class WebsiteUser(HttpLocust):
     task_set = UserTasks
